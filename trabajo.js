f3d4adadbdb3734aa12abbb22f771b9560856af1
Pablo <pablorax05@gmail.com>
2024-08-24 12:49:43 -0600

    Ejercicio 24.08.2024

---
+++ trabajo.js
@@ -0,0 +1,41 @@
+//For:
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+  
+//If:  
+  let number = 12;
+
+if (number > 10) {
+  console.log("El número es mayor que 10");
+}
+
+//while:
+let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+
+//if-else:
+let num = 7;
+
+if (num % 2 === 0) {
+  console.log("El número es par");
+} else {
+  console.log("El número es impar");
+}
+
+//Funsión:
+function sumar(a, b) {
+    return a + b;
+  }
+  
+  let resultado = sumar(3, 4);
+  console.log("La suma es:", resultado);
+  
+
+
+
